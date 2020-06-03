3f4c7f8402b12f2f61b67da196ff2511213b22d9
Vincent Roth <vincent@placeme.io>
2020-06-03 16:46:23 +0200

    Routes enfants

--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -5,14 +5,17 @@ import { AnimalListComponent } from './animal-list/animal-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { AnimalItemComponent } from './animal-item/animal-item.component';
 import { RouterModule } from '@angular/router';
+import { AnimalRoutingModule } from './animal-routing.module';
+import { AnimalRootComponent } from './animal-root/animal-root.component';
 
 @NgModule({
   declarations: [
     AnimalDetailComponent,
     AnimalListComponent,
     AnimalItemComponent,
+    AnimalRootComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
-  exports: [AnimalDetailComponent, AnimalListComponent],
+  imports: [CommonModule, SharedModule, AnimalRoutingModule],
+  exports: [],
 })
 export class AnimalModule {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AnimalListComponent } from './animal/animal-list/animal-list.component';
-import { AnimalDetailComponent } from './animal/animal-detail/animal-detail.component';
 
-const routes: Routes = [
-  { path: '', component: AnimalListComponent },
-  { path: 'animals/:id', component: AnimalDetailComponent },
-];
+const routes: Routes = [{ path: '', redirectTo: 'animals', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
