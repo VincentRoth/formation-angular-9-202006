fc37d0727e227d6fd0eb36d00c1997812f6682c1
Vincent Roth <vincent@placeme.io>
2020-06-04 17:16:54 +0200

    Formulaire réactif - validateur

--- src/app/vet/vet-form/vet-form.component.ts
+++ src/app/vet/vet-form/vet-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Veterinarian } from '../../shared/api/veterinarian';
 import { VeterinarianService } from '../../shared/api/veterinarian.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -22,38 +22,39 @@ export class VetFormComponent implements OnInit {
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
 
+    this.vetForm = new FormGroup({
+      firstName: new FormControl(null, [Validators.required]),
+      lastName: new FormControl(null, [Validators.required]),
+    });
+
     if (id) {
       this.vetService.get(Number(id)).subscribe((vet) => {
         this.vet = vet;
-        this.vetForm = new FormGroup({
-          firstName: new FormControl(vet.firstName),
-          lastName: new FormControl(vet.lastName),
-        });
-      });
-    } else {
-      this.vetForm = new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl(),
+
+        this.vetForm.get('firstName').setValue(vet.firstName);
+        this.vetForm.get('lastName').setValue(vet.lastName);
       });
     }
   }
 
   onSubmit(): void {
-    const formModel = this.vetForm.value;
-    const vet: Veterinarian = {
-      firstName: formModel.firstName as string,
-      lastName: formModel.lastName as string,
-    };
+    if (this.vetForm.valid) {
+      const formModel = this.vetForm.value;
+      const vet: Veterinarian = {
+        firstName: formModel.firstName as string,
+        lastName: formModel.lastName as string,
+      };
 
-    const onNext = () => {
-      this.router.navigate(['/veterinarians']);
-    };
+      const onNext = () => {
+        this.router.navigate(['/veterinarians']);
+      };
 
-    if (this.vet) {
-      vet.id = this.vet.id;
-      this.vetService.update(vet).subscribe(onNext);
-    } else {
-      this.vetService.create(vet).subscribe(onNext);
+      if (this.vet) {
+        vet.id = this.vet.id;
+        this.vetService.update(vet).subscribe(onNext);
+      } else {
+        this.vetService.create(vet).subscribe(onNext);
+      }
     }
   }
 }
